7e858ad42e89c3dead367edbdacc46acdfab2af1
anneett <ann_oganesyan59@mail.ru>
2023-12-11 14:32:59 +0300

    добавлен хэдер

--- main/main_oganesyan.cpp
+++ main/main_oganesyan.cpp
@@ -1,8 +1,98 @@
 #include <iostream>
-
+#include <vector>
+#include "amirova.h"
+#include "team_oganesyan.h"
+#include "grigorova.h"
+#include "Ankudoviche.h"
 using namespace std;
 
 int main()
 {
-	cout << "created main file" << endl;
+    GTS gts;
+    vector<vector<int>> vecOfVec;
+    vector<Edge2> vecOfVec2;
+    vector<Edge1> vecOfVec1;
+
+
+    cout << "Enter the number of vertices: ";
+    int V;
+    V = GetCorrectInt(1, 100);
+    Graph1 minigraph(V);
+    vector<vector<int>> graph2(V, vector<int>(V, 0));
+
+    int a;
+    cout << "Enter the number of edges: ";
+    a = GetCorrectInt(1, 200);
+
+    for (int i = 0; i < a; i++) {
+        vector<int> tempVec;
+        Edge2 e2;
+        Edge1 e1;
+        for (int j = 0; j < 2; j++) {
+            int num;
+            if (j == 0)
+            {
+                cout << "Enter starting vertice" << " of " << i + 1 << " edge: ";
+                int vvod1;
+                vvod1 = GetCorrectInt(0, V - 1);
+                e2.src = vvod1;
+                e1.src = vvod1;
+                num = vvod1;
+                tempVec.push_back(num);
+            }
+            if (j == 1)
+            {
+                cout << "Enter end vertice" << " of " << i + 1 << " edge: ";
+                int vvod2;
+                vvod2 = GetCorrectInt(0, V - 1);
+                e2.dest = vvod2;
+                e1.dest = vvod2;
+                num = vvod2;
+                tempVec.push_back(num);
+            }
+        }
+        vecOfVec.push_back(tempVec);
+        vecOfVec2.push_back(e2);
+        vecOfVec1.push_back(e1);
+        minigraph.addEdge(e2.src, e2.dest);
+    }
+    //{ {0, 1}, { 1, 2 }, { 2, 3 }, { 2, 4 }, { 3, 4 }, { 4, 5 }, { 3, 5 }, { 6, 7 }, { 6, 8 }, { 7, 8 }, { 4, 8 }, { 7, 5 } };
+
+    vector<vector<int>> edges_vector = vecOfVec;
+    unordered_map <int, int> colors_of_vertixes;
+    cout << endl << "Jones Plassman (Amirova)" << endl;
+    colors_of_vertixes = gts.Jones_Plassman(V, edges_vector);
+    for (auto i : colors_of_vertixes) cout << "Vertex " << i.first << " is colored with color " << i.second << endl;
+
+    vector <Edge2> edges = vecOfVec2;
+    int numcolors = 0;
+    /*vector<int> colors(V, -1);*/
+    vector <int> colors;
+    int vertex = 0;
+    cout << "\ntkacheva" << endl;
+    colors = gts.graphcoloring(edges, numcolors, V);
+    cout << "Number of colors: " << numcolors << endl;
+    cout << "----------------------" << endl;
+    for (int i = 0; i < colors.size(); i++) {
+        cout << "Vertex " << i << " -> Color: " << colors[i] << endl;
+    }
+    cout << "\ngerdt" << endl;
+    gts.welsh_powell(edges, V);
+
+    vector<Edge1> edges1 = vecOfVec1;
+    Graph graph(edges1, V);
+
+    cout << "grigorova" << endl;
+
+    cout << "Step-by-step vertex coloring of a graph:" << endl
+        << "***********************************" << endl;
+    gts.colorGraph(graph, V);
+    cout << "***********************************" << endl;
+
+    cout << "ankudovich" << endl;
+    cout << "Coloring of graph:" << endl;
+
+    minigraph.greedyColoring();
+
+    return 0;
 }
---
+++ main/main_oganesyan.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include "amirova.h"
+#include "grigorova.h"
+
+using namespace std;
+
+template <typename T>
+T GetCorrectInt(T min, T max)
+{
+	T x;
+	while ((cin >> x).fail() || (cin).peek() != '\n' || x < min || x > max)
+	{
+		cin.clear();
+		cin.ignore(100000, '\n');
+		cout << "\nEnter the correct data! Type integer number (" << min << " - " << max << "):";
+		cout << "\nEnter: ";
+	}
+	return x;
+}
+
+struct Edge2 {
+	int src, dest;
+};
+
+class GTS
+{
+public:
+	vector<int> graphcoloring(vector<Edge2>& edges, int& numcolors, int numvertices);
+
+	unordered_map<int, int> Jones_Plassman(int vertex_number, vector<vector<int>> edges_vector);
+
+	void welsh_powell(vector<Edge2> edges, int V);
+
+	void colorGraph(Graph const& graph, int n);
+
+	//void addEdge(int v, int w);
+
+	//void greedyColoring(vector<vector<int>>& graph, int V);
+};
